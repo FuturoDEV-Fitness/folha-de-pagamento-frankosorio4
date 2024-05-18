b17370ebe2d094571224de7923fa8d022d031121
Frank Osorio <frankosorio4@gmail.com>
2024-05-18 16:41:47 -0300

    adding function calculo_imposto_renda

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const calcularInss = require('./calculo_inss')
+const calculoImpostoRenda = require('./calculo_imposto_renda')
 
-const salario = 1000;
-console.log(calcularInss(salario))
+const salario = 2113;
+
+console.log("Salario bruto: ", salario)
+console.log("INSS: ", calcularInss(salario))
+console.log("Imposto renda: ", calculoImpostoRenda(salario))

181c090a8f423f1675e3458b42445d21814e2f96
Frank Osorio <frankosorio4@gmail.com>
2024-05-18 18:04:57 -0300

    adding function calculo_salario_liquido

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const calcularInss = require('./calculo_inss')
 const calculoImpostoRenda = require('./calculo_imposto_renda')
+const calculoSalarioLiquido = require('./calculo_salario_liquido')
 
-const salario = 2113;
+const salario = 1550;
 
 console.log("Salario bruto: ", salario)
 console.log("INSS: ", calcularInss(salario))
-console.log("Imposto renda: ", calculoImpostoRenda(salario))
+console.log("Imposto Renda: ", calculoImpostoRenda(salario))
+console.log("Salario Liquido: ", calculoSalarioLiquido(salario))

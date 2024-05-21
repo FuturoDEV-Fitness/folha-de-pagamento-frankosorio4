a93af68449bf1ad04558356f36a05429b7c3eef1
Frank Osorio <frankosorio4@gmail.com>
2024-05-20 23:49:37 -0300

    adding function to create PDF

--- src/calculo_salario_liquido.js
+++ src/calculo_salario_liquido.js
@@ -2,7 +2,7 @@ const calcularInss = require('./calculo_inss')
 const calculoImpostoRenda = require('./calculo_imposto_renda')
 
 function calculoSalarioLiquido(salarioBruto){
-    return salarioBruto - calcularInss(salarioBruto) - calculoImpostoRenda(salarioBruto)
+    return (salarioBruto - calcularInss(salarioBruto) - calculoImpostoRenda(salarioBruto)).toFixed(2)
 }
 
 module.exports = calculoSalarioLiquido;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const readline = require('readline');
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
+const { format } = require('date-fns');
 const calcularInss = require('./calculo_inss');
 const calculoImpostoRenda = require('./calculo_imposto_renda');
 const calculoSalarioLiquido = require('./calculo_salario_liquido');
@@ -19,26 +22,31 @@ function obterData(questionText) {
 }
 
 async function main() {
-    nomeEmpregado = await obterData("Digite o nome do Empregado: ");
-
     while (true) {
-        cpfEmpregado = await obterData("Digite o cpf do Empregado (só números): ");
-        if (!isNaN(cpfEmpregado) && Number(cpfEmpregado) > 0 && cpfEmpregado.length === 11) {
+        nomeEmpregado = await obterData("Digite o NOME do Empregado: ");
+        if (nomeEmpregado != '' && nomeEmpregado.length > 3) {
             break;
         }
-        console.log("CPF inválido. O CPF deve ser um número de 11 dígitos maior que zero.");
+        console.log("NOME INVALIDO! Digite um nome valido.");
     }
 
     while (true) {
-        const salario = await obterData("Digite o salário bruto do Empregado (só números): ");
+        cpfEmpregado = await obterData("Digite o CPF do Empregado (só números): ");
+        if (!isNaN(cpfEmpregado) && Number(cpfEmpregado) > 0 && cpfEmpregado.length === 11) {
+            cpfEmpregado = cpfEmpregado.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+            break;
+        }
+        console.log("CPF INVALIDO! O CPF deve ser um número de 11 dígitos maior que zero.");
+    }
+
+    while (true) {
+        const salario = await obterData("Digite o SALRIO bruto do Empregado (só números): ");
         if (!isNaN(salario) && Number(salario) > 0) {
             salarioBruto = Number(salario);
             break;
         }
-        console.log("Salário deve ser um número maior que zero.");
+        console.log("SALARIO INVALIDO! Salário deve ser um número maior que zero.");
     }
-
-    input.close();
 
     const INSS = calcularInss(salarioBruto);
     const impostoRenda = calculoImpostoRenda(salarioBruto);
@@ -51,45 +59,35 @@ async function main() {
     console.log(`INSS: R$ ${INSS}`);
     console.log(`Imposto Renda: R$ ${impostoRenda}`);
     console.log(`Salário Líquido: R$ ${salarioLiquido}`);
+
+    while (true) {
+        const desition = await obterData("Deseja crear um pdf para a folha de pagamento? \nDigite \"sim\" ou \"nao\": ");
+        if (desition.toLowerCase() === 'sim' || desition.toLowerCase() === 's') {
+            // createPdf()
+            const now = new Date;
+            const doc = new PDFDocument();
+            doc.pipe(fs.createWriteStream('folha_pagamento.pdf'));
+            doc.fontSize(14);
+            doc.text("--- Folha de pagamento ---");
+            doc.text(`Data de geração: ${format(now, 'dd-MM-yyyy')}`);
+            doc.text(`Nome: ${nomeEmpregado}`);
+            doc.text(`CPF: ${cpfEmpregado}`);
+            doc.text("--- ---");
+            doc.text(`Salário bruto: R$ ${salarioBruto.toFixed(2)}`);
+            doc.text("--- ---");
+            doc.text(`INSS: R$ ${INSS}`);
+            doc.text(`Imposto Renda: R$ ${impostoRenda}`);
+            doc.text(`Outros descontos: R$ 0.00`);
+            doc.text("--- ---");
+            doc.text(`Salário Líquido: R$ ${salarioLiquido}`);
+            doc.end();
+            console.log('Folha de pagamento salva em pdf');
+            break;
+        } else if (desition.toLowerCase() === 'nao' || desition.toLowerCase() === 'n') {
+            break;
+        }
+    }
+    input.close();
 }
 
 main();
-
-//------------------------ last-------------------------
-// let nomeEmpregado = "";
-// let cpfEmpregado = "";
-// let salarioBruto = 0;
-
-// input.question("Digite o nome do Empregado: ",(nome) =>{
-//     nomeEmpregado = nome.trim();
-//     input.question("Digite o cpf do Empregado(só numeros): ",(cpf) =>{
-
-//         if(isNaN(cpf) || Number(cpf.trim()) <= 0 || !(cpf.length === 11)){
-//             console.log("cpf Invalido");
-//             input.close();
-//             return;
-//         }
-//         cpfEmpregado = cpf.trim();
-//         input.question("Digite o salario bruto do Empregado(só numeros): ",(salario) =>{
-//             if(isNaN(salario) || Number(salario.trim()) <= 0){
-//                 console.log("Salario debe ser um numero maio que zero!");
-//                 input.close();
-//                 return;
-//             }
-//             salarioBruto = Number(salario.trim());
-//             input.close();
-
-//             let INSS = calcularInss(salarioBruto);
-//             let impostoRenda = calculoImpostoRenda(salarioBruto);
-//             let salarioLiquido = calculoSalarioLiquido(salarioBruto);
-            
-//             console.log("--- Folha de pagamento ---");
-//             console.log(`Nome: ${nomeEmpregado}`);
-//             console.log(`CPF: ${cpfEmpregado}`);
-//             console.log(`Salário bruto: R$ ${salarioBruto}`);
-//             console.log(`INSS: R$ ${INSS}`);
-//             console.log(`Imposto Renda: R$ ${impostoRenda}`);
-//             console.log(`Salário Líquido: R$ ${salarioLiquido}`);
-//         });
-//     });
-// });
